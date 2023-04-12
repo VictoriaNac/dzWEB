86f6ad68e0145c5abb5af4939a7579dededc941c
Viktoria <android1986mir@rambler.ru>
2023-04-12 10:38:50 +0300

    dz

--- exersize1/1.js
+++ exersize1/1.js
@@ -4,7 +4,6 @@
 //с текстом (пример): Цельсий: 21, Фаренгейт: 69.8
 
 const firstNum = Number.parseFloat(prompt("Введите температуру в градусах Цельсия :"));
-const secondNum = Number.parseFloat((firstNum*(9/5)) + 32);
 alert(`Цельсий: ${firstNum}, Фаренгейт: ${((firstNum*(9/5)) + 32).toFixed(0)}`);
 
 
